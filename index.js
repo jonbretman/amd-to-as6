cff909eef3f7c1b022ca81e9186fa683671b6750
Simen Bekkhus <sbekkhus91@gmail.com>
2015-07-28 10:32:33 +0200

    Strip 'use strict' if present

--- index.js
+++ index.js
@@ -49,6 +49,10 @@ function convert (source, options) {
             throw new Error('Dynamic module names are not supported.');
         }
 
+        if (isUseStrict(node)) {
+          node.parent.update('');
+        }
+
     });
 
     // no module definition found - return source untouched
@@ -338,4 +342,13 @@ function isDefineUsingIdentifier(node) {
  */
 function makeImportName (moduleName) {
     return '$__' + moduleName.replace(/-/g, '_').replace(/\//g, '_');
+}
+
+/**
+ * Returns true if node represents a 'use strict'-statement
+ * @param {object} node
+ * @returns {boolean}
+ */
+function isUseStrict (node) {
+    return node.type === 'Literal' && node.value === 'use strict';
 }
